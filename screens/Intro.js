f96bf52513c4b9e3ff6bd6c30fc3c3aa94297fcf
Gerson Montenegro <montenegrogerson@gmail.com>
2018-07-30 14:47:07 +1200

    Puting a circle component instead an image with a circle. Yes, y more difficult, but I'd rather than an pixel shape

--- screens/Intro.js
+++ screens/Intro.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Image, View, Text } from 'react-native';
 import { screenWidth } from './../assets/css/general';
+import Circle from './../components/primitives/Circle';
 
 class Intro extends PureComponent{
   constructor(props){
@@ -8,6 +9,7 @@ class Intro extends PureComponent{
   }
 
   render(){
+    //<Image source={require('./../assets/img/face.png')} style={{width: 150, resizeMode: 'contain', marginTop: 50}} />
     return (
       <View style={{marginTop: 0}}>
         <View style={{
@@ -19,7 +21,8 @@ class Intro extends PureComponent{
           justifyContent: 'center',
           alignItems: 'center'
         }}>
-          <Image source={require('./../assets/img/face.png')} style={{width: 150, resizeMode: 'contain', marginTop: 50}} />
+          <Circle width={150} height={150} color="white" borderWidth={8} />
+          <Image source={require('./../assets/img/grs.png')} style={{width: 100, height: 100, resizeMode: 'contain', marginTop: -125}} />
         </View>
         <View style={{width: screenWidth, justifyContent: 'center', alignItems: 'center', marginTop: 50}}>
           <Text style={{marginTop: 0, fontFamily: 'CocoGothic-Bold', fontSize: 25}}>
