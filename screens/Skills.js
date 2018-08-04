a79593fed77266b100e61b360ab3adb1e4725264
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-04 22:46:47 +1200

    Add new PieDetail component and remove previous code where the new component is created

--- screens/Skills.js
+++ screens/Skills.js
@@ -3,30 +3,13 @@ import { ScrollView, Image, View, Text } from 'react-native';
 import { screenWidth } from './../assets/css/general';
 import Circle from './../components/primitives/Circle';
 import { VictoryPie } from 'victory-native';
+import PieDetail from './../components/PieDetail';
 
 const timer = require('react-native-timer');
 
 class Skills extends PureComponent{
   constructor(props){
     super(props);
-    this.state = { percent: 70, data: this.getData(70) };
-    // this.startTimer();
-  }
-
-  startTimer(){
-    timer.setInterval(this, '', () => {
-      if(this.state.percent < 100){
-        this.setState( { percent: this.state.percent + 0.5, data: this.getData( this.state.percent + 0.5 ) } )
-      }
-    }, 10);
-  }
-
-  componentWillUnmount(){
-    timer.clearInterval(this);
-  }
-
-  getData(percent){
-    return [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
   }
 
   render(){
@@ -41,32 +24,11 @@ class Skills extends PureComponent{
         </View>
         <View style={{marginTop: 0, marginLeft: 5, marginRight: 5}}>
           <ScrollView style={{marginTop: 20}}>
-            <View style={{flexDirection: 'row'}}>
-              <VictoryPie
-                width={100}
-                height={100}
-                data={this.state.data}
-                innerRadius={40}
-                cornerRadius={2}
-                labels={() => null}
-                style={{
-                  data: {
-                    fill: (d) => {
-                      const color = d.y > 30 ? "green" : "red";
-                      return d.x === 1 ? color : "transparent"
-                    }
-                  }
-                }}
-               />
-               <View style={{justifyContent: 'center'}}>
-                <Text>
-                  Ionic
-                </Text>
-                <Text>
-                  iOS and Android developer
-                </Text>
-               </View>
-            </View>
+            <PieDetail percent={95} pie_title="ActionScript" pie_description="iOS, Android, and Desktop developer" />
+            <PieDetail percent={70} pie_title="Ionic" pie_description="iOS and Android developer" />
+            <PieDetail percent={50} pie_title="React-Native" pie_description="iOS and Android developer" />
+            <PieDetail percent={70} pie_title="PHP" pie_description="Web developer" />
+            <PieDetail percent={60} pie_title="Laravel" pie_description="PHP developer" />
           </ScrollView>
           <View style={{position: 'absolute', alignItems: 'center', marginLeft: (screenWidth/2) - 45, marginTop: -115}}>
             <Circle width={90} height={90} color="white" borderWidth={5} marginTop={40} />
