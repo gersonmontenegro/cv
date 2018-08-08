7ad3c2c10ad385652df74861bdc3e674e86bc1b0
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-08 20:52:47 +1200

    Adding variable with the height of the screen

--- assets/css/general.js
+++ assets/css/general.js
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 import { Dimensions } from 'react-native';
 
 export const screenWidth = Dimensions.get('window').width;
+export const screenHeight = Dimensions.get('window').height;
 
 export const HeaderStyle = StyleSheet.create({
   headerContainer: {
