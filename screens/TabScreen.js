41b8d88a4d129cfea053577f04effc7b3dc1bf8a
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-08 20:53:37 +1200

    Preliminary view of the Tab menu with an interface charged

--- screens/TabScreen.js
+++ screens/TabScreen.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
-import { View } from 'react-native';
+// import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
+import { Footer, Content, FooterTab, Button, Icon } from 'native-base';
+import { ScrollView, Image, View, Text } from 'react-native';
 // import Icon from 'react-native-vector-icons/Ionicons';
+import Circle from './../components/primitives/Circle';
 import Profile from './Profile';
 import Skills from './Skills';
-import { screenWidth } from './../assets/css/general.js';
+import Education from './Education';
+import { screenWidth, screenHeight } from './../assets/css/general.js';
 
 const footerFontSize = 12;
+const footerHeight = 70;
 const profileColor = '#1fb9d7';
 const skillsColor = '#3071ae';
 const educationColor = '#4ab04a';
@@ -19,9 +23,14 @@ class TabScreen extends Component{
 
   render(){
     return (
-      <Container style={{ width: screenWidth }}>
-        <Header />
-        <Content />
+      <View style={{justifyContent: 'flex-start', flex: 1}}>
+        <Content>
+          <View style={{marginTop: 0, marginLeft: 0, marginRight: 5}}>
+            <View style={{marginTop: 20}}>
+              <Education />
+            </View>
+          </View>
+        </Content>
         <Footer>
           <FooterTab>
             <Button vertical>
@@ -40,7 +49,7 @@ class TabScreen extends Component{
                 </View>
               </View>
             </Button>
-            <Button vertical>
+            <Button active vertical>
               <Icon name="ios-book" />
               <Text style={{ fontFamily: 'CenturyGothic', fontSize: footerFontSize }}>Educat.</Text>
               <View style={{ marginBottom: -6 }}>
@@ -48,7 +57,7 @@ class TabScreen extends Component{
                 </View>
               </View>
             </Button>
-            <Button active vertical>
+            <Button vertical>
               <Icon name="ios-construct" />
               <Text style={{ fontFamily: 'CenturyGothic', fontSize: footerFontSize }}>Exper.</Text>
               <View style={{ marginBottom: -6 }}>
@@ -58,10 +67,9 @@ class TabScreen extends Component{
             </Button>
           </FooterTab>
         </Footer>
-      </Container>
+      </View>
     );
   }
-
 }
 
 export default TabScreen;
