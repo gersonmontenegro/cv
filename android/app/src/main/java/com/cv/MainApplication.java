eee79174f3f7ce88b8aa9cedde288cc3d5a9fba6
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-08 20:30:17 +1200

    Adding react-native-vector-icons to Android project

--- android/app/src/main/java/com/cv/MainApplication.java
+++ android/app/src/main/java/com/cv/MainApplication.java
@@ -3,6 +3,7 @@ package com.cv;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.github.wuxudong.rncharts.MPAndroidChartPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new SvgPackage(),
             new MPAndroidChartPackage()
       );
