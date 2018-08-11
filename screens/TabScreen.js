72933e3026bd23753aad784f1e2d2283809466d5
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-11 20:26:01 +1200

    Adding funcionality at the tab menu, changing and loading the current interface

--- screens/TabScreen.js
+++ screens/TabScreen.js
@@ -7,6 +7,7 @@ import Circle from './../components/primitives/Circle';
 import Profile from './Profile';
 import Skills from './Skills';
 import Education from './Education';
+import Experience from './Experience';
 import { screenWidth, screenHeight } from './../assets/css/general.js';
 
 const footerFontSize = 12;
@@ -19,21 +20,59 @@ const experiencieColor = '#f86236';
 class TabScreen extends Component{
   constructor(props){
     super(props);
+    this.renderComponent = this.renderComponent.bind(this);
+    this.state = { currentInterface: 1 };
+  }
+
+  renderComponent = () =>{
+    switch(this.state.currentInterface){
+      case 1: {
+        return <Profile />
+        break;
+      }
+      case 2: {
+        return <Skills />
+        break;
+      }
+      case 3: {
+        return <Education />
+        break;
+      }
+      case 4: {
+        return <Experience />
+        break;
+      }
+      default:{
+        console.log("wot!");
+      }
+    }
+  }
+
+  changeInterface = (i: Number) => {
+    this.setState({ currentInterface: i });
+  }
+
+  isButtonActive = (b: Number) => {
+    if(b == this.state.currentInterface){
+      return true;
+    }else{
+      return false;
+    }
   }
 
   render(){
     return (
-      <View style={{justifyContent: 'flex-start', flex: 1}}>
+      <View style={{justifyContent: 'flex-start', flex: 1, width: screenWidth}}>
         <Content>
           <View style={{marginTop: 0, marginLeft: 0, marginRight: 5}}>
             <View style={{marginTop: 20}}>
-              <Education />
+                {this.renderComponent()}
             </View>
           </View>
         </Content>
         <Footer>
           <FooterTab>
-            <Button vertical>
+            <Button active={this.isButtonActive(1)} vertical onPress={() => { this.changeInterface(1) }}>
               <Icon name="ios-contact" />
               <Text style={{ fontFamily: 'CenturyGothic', fontSize: footerFontSize }}>Profile</Text>
               <View style={{ marginBottom: -6 }}>
@@ -41,7 +80,7 @@ class TabScreen extends Component{
                 </View>
               </View>
             </Button>
-            <Button vertical>
+            <Button active={this.isButtonActive(2)} onPress={() => { this.changeInterface(2) }}>
               <Icon name="ios-cog" />
               <Text style={{ fontFamily: 'CenturyGothic', fontSize: footerFontSize }}>Skills</Text>
               <View style={{ marginBottom: -6 }}>
@@ -49,7 +88,7 @@ class TabScreen extends Component{
                 </View>
               </View>
             </Button>
-            <Button active vertical>
+            <Button active={this.isButtonActive(3)} vertical onPress={() => { this.changeInterface(3) }}>
               <Icon name="ios-book" />
               <Text style={{ fontFamily: 'CenturyGothic', fontSize: footerFontSize }}>Educat.</Text>
               <View style={{ marginBottom: -6 }}>
@@ -57,7 +96,7 @@ class TabScreen extends Component{
                 </View>
               </View>
             </Button>
-            <Button vertical>
+            <Button active={this.isButtonActive(4)} vertical onPress={() => { this.changeInterface(4) }}>
               <Icon name="ios-construct" />
               <Text style={{ fontFamily: 'CenturyGothic', fontSize: footerFontSize }}>Exper.</Text>
               <View style={{ marginBottom: -6 }}>
