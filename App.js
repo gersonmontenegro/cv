b346f98d81a9a450b4cb20a5ed2f864ef6daa344
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-11 20:17:37 +1200

    Removing scratch from the original template

--- App.js
+++ App.js
@@ -6,14 +6,6 @@ import Skills from './screens/Skills';
 import Education from './screens/Education';
 import TabScreen from './screens/TabScreen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
