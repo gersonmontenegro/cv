ae4445f1466531bfe125abf3485a8b85618808be
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-15 23:49:55 +1200

    Adding the main background color

--- assets/css/general.js
+++ assets/css/general.js
@@ -3,6 +3,7 @@ import { Dimensions } from 'react-native';
 
 export const screenWidth = Dimensions.get('window').width;
 export const screenHeight = Dimensions.get('window').height;
+export const mainBackgroundColor = '#2e70ac';
 
 export const HeaderStyle = StyleSheet.create({
   headerContainer: {
