94053d62ad9a2f48b15e839d59303df00e2a0546
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-16 21:30:27 +1200

    Adding contant color to the general CSS

--- assets/css/general.js
+++ assets/css/general.js
@@ -4,7 +4,11 @@ import { Dimensions } from 'react-native';
 export const screenWidth = Dimensions.get('window').width;
 export const screenHeight = Dimensions.get('window').height;
 export const mainBackgroundColor = '#2e70ac';
-export const defaultAnimationTime = 1000;
+export const defaultAnimationTime = 500;
+export const profileColor = '#1fb9d7';
+export const skillsColor = '#3071ae';
+export const educationColor = '#4ab04a';
+export const experiencieColor = '#f86236';
 
 export const HeaderStyle = StyleSheet.create({
   headerContainer: {
