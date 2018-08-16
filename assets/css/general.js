22fed3d252e072d8561f06d448e4b937841430de
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-16 20:55:16 +1200

    Adding constant for the default time animation

--- assets/css/general.js
+++ assets/css/general.js
@@ -4,6 +4,7 @@ import { Dimensions } from 'react-native';
 export const screenWidth = Dimensions.get('window').width;
 export const screenHeight = Dimensions.get('window').height;
 export const mainBackgroundColor = '#2e70ac';
+export const defaultAnimationTime = 1000;
 
 export const HeaderStyle = StyleSheet.create({
   headerContainer: {
