8ae30165fef077c79453a3824984e6e802dd6a04
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-20 00:27:29 +1200

    First version of the new header. The change worth it :)

--- screens/TabScreen.js
+++ screens/TabScreen.js
@@ -30,6 +30,10 @@ class TabScreen extends Component{
     this.nameOpacity = new Animated.Value(1);
     this.nameWidth = new Animated.Value((screenWidth - (screenWidth * .3)));
     this.titleWidth = new Animated.Value((screenWidth - (screenWidth * .5)));
+    this.marginTopAvatar = new Animated.Value(0);
+    this.marginLeftAvatar = new Animated.Value(0);
+    this.avatarWidth = new Animated.Value(80);
+    this.avatarHeight = new Animated.Value(100);
   }
 
   componentDidMount(){
@@ -115,48 +119,48 @@ class TabScreen extends Component{
   render(){
     return (
       <View style={{justifyContent: 'flex-start', flex: 1, width: screenWidth}}>
-        <Header style={{backgroundColor: mainBackgroundColor, height: 100}}>
+        <Header style={{backgroundColor: mainBackgroundColor, height: 120}}>
           <View style={{
-            width: screenWidth,
-            alignItems: 'center'
+            width: screenWidth * 1.5,
+            height: 103,
+            backgroundColor: mainBackgroundColor
           }}>
-          <Animated.Image
+          <Image
             source={require('./../assets/img/grs_pixel_with_circle_200.png')}
             style={{
-              marginTop: this.marginTopAvatar,
-              marginLeft: this.marginLeftAvatar,
-              width: this.avatarWidth,
-              height: this.avatarHeight,
-              alignSelf: 'center',
+              marginTop: 7,
+              marginLeft: 92,
+              width: 80,
+              height: 80,
+              alignSelf: 'flex-start',
+              position: 'absolute'
             }}
           />
-          <Animated.View
+          <View
             style={{
-              width: this.nameWidth,
               height: 25,
-              position: 'absolute',
               backgroundColor: 'white',
-              marginRight: 10,
-              marginTop: 35,
+              marginTop: 17,
               alignSelf: 'flex-end',
               borderRadius: 10,
+              marginRight: 78,
               justifyContent: 'center',
+              width: this.nameWidth,
               opacity: this.nameOpacity
             }}
           >
             <Text style={{ alignSelf: 'flex-end', fontFamily: 'CocoGothic-Bold', marginRight: 15, fontSize: 16}}>
               Gerson Montenegro
             </Text>
-          </Animated.View>
-          <Animated.View
+          </View>
+          <View
             style={{
               opacity: this.titleOpacity,
               width: this.titleWidth,
               height: 25,
-              position: 'absolute',
               backgroundColor: 'white',
-              marginRight: 10,
-              marginTop: 65,
+              marginRight: 78,
+              marginTop: 5,
               alignSelf: 'flex-end',
               borderRadius: 10,
               justifyContent: 'center'
@@ -165,7 +169,7 @@ class TabScreen extends Component{
             <Text style={{ alignSelf: 'flex-end', fontFamily: 'CocoGothic', marginRight: 15 }}>
               Mobile developer
             </Text>
-           </Animated.View>
+           </View>
           </View>
         </Header>
         <Content>
