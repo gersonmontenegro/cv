bf70e0e86d40b263a1403bedc068b1d022eb7af4
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-21 23:42:43 +1200

    Adding texts and texts containers styles

--- assets/css/general.js
+++ assets/css/general.js
@@ -12,10 +12,52 @@ export const educationColor = '#4ab04a';
 export const experiencieColor = '#f86236';
 export const finalAvatarDimension = 80;
 
+const nameWidth: numner = screenWidth - (screenWidth * .3);
+const titleWidth: numner = screenWidth - (screenWidth * .5);
+
 export const HeaderStyle = StyleSheet.create({
   headerContainer: {
     width: screenWidth,
     height: 102,
-    backgroundColor: mainBackgroundColor
+    backgroundColor: mainBackgroundColor,
+    flexDirection: 'row'
+  },
+  avatarStyle: {
+    marginTop: 7,
+    marginLeft: 12,
+    width: 80,
+    height: 80,
+  },
+  textsContainer: {
+    width: (screenWidth - 90),
+    alignItems: 'flex-end',
+    flexDirection: 'column'
+  },
+  nameStyle: {
+    height: 25,
+    width: nameWidth,
+    backgroundColor: 'white',
+    marginTop: 17,
+    borderRadius: 10,
+    justifyContent: 'center'
+  },
+  nameTextStyle: {
+    alignSelf: 'flex-end',
+    fontFamily: 'CocoGothic-Bold',
+    marginRight: 15,
+    fontSize: 16
+  },
+  titleStyle: {
+    width: titleWidth,
+    height: 25,
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderRadius: 10,
+    justifyContent: 'center'
+  },
+  titleTextStyle: {
+    alignSelf: 'flex-end',
+    fontFamily: 'CocoGothic',
+    marginRight: 15
   }
 });
