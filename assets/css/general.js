4ca114921c419e36f30682e7832f80e4506139aa
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-21 23:01:16 +1200

    Preparing styles to the new header

--- assets/css/general.js
+++ assets/css/general.js
@@ -4,7 +4,8 @@ import { Dimensions } from 'react-native';
 export const screenWidth = Dimensions.get('window').width;
 export const screenHeight = Dimensions.get('window').height;
 export const mainBackgroundColor = '#2e70ac';
-export const defaultAnimationTime = 500;
+export const defaultAnimationTime = 50;
+// export const defaultAnimationTime = 500;
 export const profileColor = '#1fb9d7';
 export const skillsColor = '#3071ae';
 export const educationColor = '#4ab04a';
@@ -13,6 +14,8 @@ export const finalAvatarDimension = 80;
 
 export const HeaderStyle = StyleSheet.create({
   headerContainer: {
-
+    width: screenWidth,
+    height: 102,
+    backgroundColor: mainBackgroundColor
   }
 });
