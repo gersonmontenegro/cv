b46cd4a68f073935e3a75994e53087414d424a11
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-21 23:45:37 +1200

    Removing inneesaring lines

--- assets/css/general.js
+++ assets/css/general.js
@@ -1,11 +1,10 @@
-import { StyleSheet } from 'react-native';
-import { Dimensions } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export const screenWidth = Dimensions.get('window').width;
 export const screenHeight = Dimensions.get('window').height;
 export const mainBackgroundColor = '#2e70ac';
 export const defaultAnimationTime = 50;
-// export const defaultAnimationTime = 500;
+export const defaultAnimationTime = 500;
 export const profileColor = '#1fb9d7';
 export const skillsColor = '#3071ae';
 export const educationColor = '#4ab04a';
