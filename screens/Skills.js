ed5cfd93ecc0abf90ca75c24c458bbd5014107f6
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-22 17:02:27 +1200

    Removing the scratchs, and implementing the Bars component

--- screens/Skills.js
+++ screens/Skills.js
@@ -4,6 +4,7 @@ import { screenWidth } from './../assets/css/general';
 import Circle from './../components/primitives/Circle';
 import { VictoryPie } from 'victory-native';
 import PieDetail from './../components/PieDetail';
+import Bars from './../components/charts/Bars';
 
 const timer = require('react-native-timer');
 
@@ -17,45 +18,17 @@ class Skills extends PureComponent{
       <View style={{justifyContent: 'flex-start', flex: 1}}>
         <View style={{marginTop: 0, marginLeft: 5, marginRight: 5}}>
           <ScrollView style={{marginTop: 20}}>
-            <View style={{width: screenWidth - 10, flexDirection: 'column'}}>
-              <View style={{width: screenWidth - 15, borderRadius: 5}}>
-                <Text style={{fontFamily: 'CocoGothic-Bold', marginLeft: 5}}>
-                  ActionScript
-                </Text>
-              </View>
-              <View style={{width: screenWidth - 15, backgroundColor: 'lightblue', borderRadius: 5}}>
-                <View style={{height: 20, width: screenWidth - 15, backgroundColor: '#7E98B0', borderRadius: 5, opacity: 0.3}}>
-                </View>
-                <View style={{alignItems: 'center', position: 'absolute', height: 20, width: screenWidth - 50, backgroundColor: '#7E98B0', borderRadius: 5}}>
-                  <Text style={{position: 'absolute', color: '#000', fontFamily: 'CenturyGothic-Bold'}}>
-                    90%
-                  </Text>
-                </View>
-              </View>
-            </View>
+            <Bars name='ActionScript' percentaje={90} primaryColor='#db4e4e' />
+            <Bars name='Ionic' percentaje={70} primaryColor='#2f9cfc' />
+            <Bars name='PHP' percentaje={75} primaryColor='#627dbc' />
+            <Bars name='Laravel' percentaje={50} primaryColor='#f0514b' />
+            <Bars name='React Native' percentaje={30} primaryColor='#1ca6cd' />
 
-            <View style={{width: screenWidth - 10, flexDirection: 'column', marginTop: 5}}>
-              <View style={{width: screenWidth - 15, borderRadius: 5}}>
-                <Text style={{fontFamily: 'CocoGothic-Bold', marginLeft: 5}}>
-                  Ionic
-                </Text>
-              </View>
-              <View style={{width: screenWidth - 15, backgroundColor: 'lightblue', borderRadius: 5}}>
-                <View style={{height: 20, width: screenWidth - 15, backgroundColor: '#FCBD24', borderRadius: 5, opacity: 0.3}}>
-                </View>
-                <View style={{alignItems: 'center', position: 'absolute', height: 20, width: screenWidth - 90, backgroundColor: '#FCBD24', borderRadius: 5}}>
-                  <Text style={{position: 'absolute', color: '#000', fontFamily: 'CenturyGothic-Bold'}}>
-                    70%
-                  </Text>
-                </View>
-              </View>
-            </View>
-
-            <PieDetail color="green" percent={95} pie_title="ActionScript" pie_description="iOS, Android, and Desktop developer" />
+            {/* <PieDetail color="green" percent={95} pie_title="ActionScript" pie_description="iOS, Android, and Desktop developer" />
             <PieDetail color="lightblue" percent={70} pie_title="Ionic" pie_description="iOS and Android developer" />
             <PieDetail color="blue" percent={50} pie_title="React-Native" pie_description="iOS and Android developer" />
             <PieDetail color="orange" percent={70} pie_title="PHP" pie_description="Web developer" />
-            <PieDetail color="red" percent={50} pie_title="Laravel" pie_description="PHP developer" />
+            <PieDetail color="red" percent={50} pie_title="Laravel" pie_description="PHP developer" /> */}
           </ScrollView>
         </View>
       </View>
