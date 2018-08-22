f22c56afcf4550b835cd1a945218e39d76380d03
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-22 16:18:53 +1200

    Creating from scratchs kind of bars.

--- screens/Skills.js
+++ screens/Skills.js
@@ -17,6 +17,22 @@ class Skills extends PureComponent{
       <View style={{justifyContent: 'flex-start', flex: 1}}>
         <View style={{marginTop: 0, marginLeft: 5, marginRight: 5}}>
           <ScrollView style={{marginTop: 20}}>
+            <View style={{width: screenWidth - 10, flexDirection: 'column'}}>
+              <View style={{width: screenWidth - 15, borderRadius: 5}}>
+                <Text style={{fontFamily: 'CocoGothic-Bold', marginLeft: 5}}>
+                  ActionScript
+                </Text>
+              </View>
+              <View style={{width: screenWidth - 15, backgroundColor: 'lightblue', borderRadius: 5}}>
+                <View style={{height: 20, width: screenWidth - 15, backgroundColor: '#7E98B0', borderRadius: 5, opacity: 0.3}}>
+                </View>
+                <View style={{alignItems: 'center', position: 'absolute', height: 20, width: screenWidth - 50, backgroundColor: '#7E98B0', borderRadius: 5}}>
+                  <Text style={{position: 'absolute', color: '#000', fontFamily: 'CenturyGothic-Bold'}}>
+                    90%
+                  </Text>
+                </View>
+              </View>
+            </View>
             <PieDetail color="green" percent={95} pie_title="ActionScript" pie_description="iOS, Android, and Desktop developer" />
             <PieDetail color="lightblue" percent={70} pie_title="Ionic" pie_description="iOS and Android developer" />
             <PieDetail color="blue" percent={50} pie_title="React-Native" pie_description="iOS and Android developer" />
