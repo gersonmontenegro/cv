ae8a9b9b688a48e31b6aa9e630e067863a56b65f
Gerson Montenegro <montenegrogerson@gmail.com>
2018-08-25 02:10:26 +1200

    Removing old Bar component implementation, and calling the new method that returns the Bar component list.

--- screens/Skills.js
+++ screens/Skills.js
@@ -35,16 +35,13 @@ class Skills extends PureComponent{
       }
     );
   }
+
   render(){
     return (
       <View style={{justifyContent: 'flex-start', flex: 1}}>
         <View style={{marginTop: 0, marginLeft: 5, marginRight: 5}}>
           <ScrollView style={{marginTop: 20}}>
-            <Bars name='ActionScript' startAt={0} percentaje={90} primaryColor='#db4e4e' />
-            <Bars name='Ionic' startAt={90} percentaje={70} primaryColor='#2f9cfc' />
-            <Bars name='PHP' startAt={180} percentaje={75} primaryColor='#627dbc' />
-            <Bars name='Laravel' startAt={270} percentaje={50} primaryColor='#f0514b' />
-            <Bars name='React Native' startAt={360} percentaje={30} primaryColor='#1ca6cd' />
+            {this.createBars()}
           </ScrollView>
         </View>
       </View>
